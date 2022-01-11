4c821f61dc90190b4d5f8f3d18085ed26dd8c7dd
Reality Reimagined <realityreimagined13@gmail.com>
2022-01-11 06:57:42 -0500

    hopefully this one fixes it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './styles.css';
+import { useEffect } from 'react';
+import NFTContainer from './NFTContainer';
+import ModelViewer from './Modelviewer';
 
-function App() {
+
+
+
+function App3() {
+  
+  const [walletAddress, setWalletAddress] = useState(null)
+  const [nfts, setNfts] = useState([])
+
+  const connectWallet = async () => {
+    if (typeof window.ethereum !== 'undefined') {
+
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    
+    setWalletAddress(accounts[0])
+
+    }
+  }
+
+  const getNFTData = async () => {
+
+    if(!walletAddress) return;
+
+  
+    const response = await fetch (`https://api.rarible.org/v0.1/items/byOwner/?owner=ETHEREUM:${walletAddress}`);
+    
+    const data = await response.json()
+
+    setNfts(data.items)
+
+    debugger
+
+  }
+
+  useEffect(() => {
+    getNFTData()
+  }, [walletAddress])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div class = "glow-on-hover" type= "button">
+       Account: {walletAddress}
+     </div>
+     <button class="glow-on-hover" type="button" onClick={connectWallet}>
+       Connect Wallet
+     </button>
+     <div>
+
+     </div>
+     <NFTContainer nfts={nfts} /> 
+     
+      <ModelViewer/>
     </div>
   );
 }
 
-export default App;
+export default App3;
